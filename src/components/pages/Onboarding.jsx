89d1ac9840104bc7cc3dd4c887f838a814f8fc49
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-12-30 20:26:12 +0000

    onboarding page added

---
+++ src/components/pages/Onboarding.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../styles/Onboarding.css";
+
+ function Onboarding() {
+  return (
+    <div>
+      <div className='onboard'>
+        
+
+      </div>
+    </div>
+  )
+}
+
+
+export default Onboarding
