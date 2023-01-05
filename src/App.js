e9b9c5c0d682ac0897393e11cd7a38362edfd027
PrinceQuarshie <prince.quarshie@amalitech.org>
2023-01-05 11:42:42 +0000

    state functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import AllServers from "./components/pages/AllServers";
 import Connected from "./components/pages/Connected";
@@ -6,27 +7,41 @@ import Disconnected from "./components/pages/Disconnected";
 import Onboarding from "./components/pages/Onboarding";
 
 function App() {
+  const [showComponent, setShowComponent] = useState(false);
+  // const [showNewComponent, setShowNewComponent] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setShowComponent(true);
+    }, 2000);
+  }, []);
+
   return (
     <div>
-      <Onboarding />
-      <Disconnected
-        title="00:00:00"
-        subtitle="Disconnected"
-        kilpersec="00 kb/s"
-        megpersec="00 mb/s"
-      />
+      {showComponent || (
       <Connecting
         title="00:00:00"
         subtitle="Connecting..."
         kilpersec="00 kb/s"
         megpersec="00 mb/s"
       />
-      <Connected
-        title="00:00:01"
-        subtitle="Connected"
-        kilpersec="256 kb/s"
-        megpersec="192 mb/s"
+      )}
+        <Connected
+          title="00:00:01"
+          subtitle="Connected"
+          kilpersec="256 kb/s"
+          megpersec="192 mb/s"
+        />
+
+      <Onboarding />
+
+      <Disconnected
+        title="00:00:00"
+        subtitle="Disconnected"
+        kilpersec="00 kb/s"
+        megpersec="00 mb/s"
       />
+
       <AllServers />
     </div>
   );
